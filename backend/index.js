9f0197df404d52541068621c5167574a167b90eb
gingerbread77 <1516519317@qq.com>
2025-04-09 15:00:31 +1200

    set up register route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const UserModel = require('./models/User')
 
 require('dotenv').config();
 
@@ -12,20 +13,36 @@ const MONGODB_URI = process.env.MONGO_URI
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+// routes
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ success: false, msg: "Missing Details" })
+  }
+  try {
+    // check whether the email is unique
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ success: false, msg: "Email already exists" })
+    }
+    const newUser = await UserModel.create({ email, password });
+    res.status(201).json({ success: true, user: newUser });
+  } catch (err) {
+    res.json({ success: false, msg: err.message })
+  }
+})
 
 // connect to database
-const connectDB = async ()=> {
+const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI);
     console.log('Connected to MONGODB');
 
-    app.listen(PORT,()=> {
+    app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`)
     })
-  } catch (err){
+  } catch (err) {
     console.error(err);
     // exit the program
     process.exit(1)
